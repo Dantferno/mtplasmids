9d1251701d61335edc8258ebdaadcc4429fa59d1
Jeroen Groeneveld <32732784+impreza-92@users.noreply.github.com>
2018-08-06 09:26:43 +0200

    Add files via upload

--- main_utils.py
+++ main_utils.py
@@ -4,6 +4,7 @@ import subprocess
 
 
 # (Optional) Function to run commands and pipe the output to the terminal 
+# Only necessary if the subprocess prints to the terminal 
 
 def run_command(command):
 	p = subprocess.Popen(command, shell=True,  stdout=subprocess.PIPE)
@@ -22,21 +23,29 @@ def run_command(command):
 
 def get_ascensions(download_directory):
 	os.chdir(download_directory)
+	# Read the ascensions from the text file
 	with open('SRR_Acc_List.txt', 'r') as f:
 		for line in f:
-			# Specify and make the output directory
+			# Specify the output directory
 			output_path = os.path.join(download_directory, line)
-			os.mkdir(output_path)
-			# Download and split the reads
-			print('Downloading & splitting: {}'.format(line))
-			subprocess.call('fastq-dump -I --split-files {0} --outdir {1}'.format(line, output_path))
-			print('{} complete!'.format(line))
+			# Check if the ascension was already downloaded
+			if os.path.exists(output_path) == True:
+				print('Ascension {} already downloaded'.format(line))
+			else:
+				# Download and split the reads
+				os.mkdir(output_path)
+				print('Downloading & splitting: {}'.format(line))
+				subprocess.call('fastq-dump -I --split-files {0} --outdir {1}'.format(line, output_path))
+				print('{} complete!'.format(line))
+		print('No more ascensions to download.')
 
 			
 # Make subsets of each paired-end library of 250k reads each
+# Seqtk manual: https://github.com/lh3/seqtk
 
 def subset(download_directory):
-	paths, directories, files = next(os.walk(os.path.join(download_directory)))
+	# Index the contents of the working directory
+	paths, directories, files = next(os.walk(download_directory))
 	for directory in directories:
 		# Go to the sample directory and check its contents
 		os.chdir(os.path.join(download_directory ,directory))
@@ -49,6 +58,12 @@ def subset(download_directory):
 		os.remove(samples[1])
 		print('250k subsets of sample {0} generated'.format(directory))
 	print('All subsets have been generated')
+
+	
+# Assemble all subsetted libraries de novo using Abyss
+# Abyss manual: https://github.com/bcgsc/abyss
+
+def assemble(download_directory):
 	
 	
 
